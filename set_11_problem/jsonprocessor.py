c294f073eb837f8df2b83edb1304a1316c4edfd6
DEBDEEPTA <deep.o8.2oo3.26@gmail.com>
2026-01-12 15:44:06 +0530

    TASK UPDATE_DAY_1:12/01/26

---
+++ set_11_problem/jsonprocessor.py
@@ -0,0 +1,37 @@
+import json
+
+from set_11_problem.file_processor import FileProcessor
+
+class JsonProcessor(FileProcessor):
+
+    def process(self,path):
+        print("processing json")
+
+    def validate(self,path):
+        print("Validating Json")
+        with open(path, "r") as f:
+            data = json.load(f) # Reading Json
+
+            if(isinstance(data, dict)):
+                return True
+            else:
+                return False
+
+    def extract(self,path):
+        print("json data")
+        with open(path, "r") as file_obj:
+            data = json.load(file_obj)
+
+            return  data
+
+    def summerize(self,path):
+        with open(path, "r") as file_obj:
+            data = json.load(file_obj)
+
+            constraints = data.get("constraints")
+
+            words = constraints.split()
+            words_count = len(words)
+            print("json summerize")
+            return words_count
+
